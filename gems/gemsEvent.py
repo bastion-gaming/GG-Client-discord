8479041fb4a60a425d61742ba0c2da547d6e12e2
Azerxim <mallet.maxime@hotmail.fr>
2020-02-20 17:50:47 +0100

    Update gemsEvent.py

--- gems/gemsEvent.py
+++ gems/gemsEvent.py
@@ -14,27 +14,14 @@ class GemsEvent(commands.Cog):
         """Date des Événements !!"""
         msg = discord.Embed(title = "Evénements", color= 13752280, description = "Date des Evénements !!")
         desc = "26 Octobre :arrow_right: 10 Novembre"
-        for one in GF.objetItem:
-            if one.type == "halloween":
-                desc += "\n<:gem_{0}:{1}>`{0}`".format(one.nom, GF.get_idmoji(one.nom))
+        desc += "\n<:gem_pumpkin:{0}>`pumpkin`".format(GF.get_idmoji("pumpkin"))
+        desc += "\n<:gem_pumpkinpie:{0}>`pumpkinpie`".format(GF.get_idmoji("pumpkinpie"))
         msg.add_field(name="Halloween", value=desc, inline=False)
 
-        desc = "14 Décembre :arrow_right: 5 Janvier"
-        for one in GF.objetItem:
-            if one.type == "christmas":
-                desc += "\n<:gem_{0}:{1}>`{0}`".format(one.nom, GF.get_idmoji(one.nom))
+        desc = "21 Décembre :arrow_right: 14 Janvier"
+        desc += "\n<:gem_cupcake:{0}>`cupcake`".format(GF.get_idmoji("cupcake"))
         desc += "\n:gift:`gift`"
         msg.add_field(name="Noël", value=desc, inline=False)
-
-        # desc = "10 Février :arrow_right: 17 Février"
-        # for one in GF.objetItem:
-        #     if one.type == "saint valentin":
-        #         desc += "\n<:gem_{0}:{1}>`{0}`".format(one.nom, GF.get_idmoji(one.nom))
-        # desc += "\n:gift_heart:`gift_heart`"
-        # msg.add_field(name="Saint Valentin", value=desc, inline=False)
-        #
-        # desc = "7 Juillet :arrow_right: 21 Juillet"
-        # msg.add_field(name="Fête Nationale", value=desc, inline=False)
         await ctx.channel.send(embed = msg)
 
 
