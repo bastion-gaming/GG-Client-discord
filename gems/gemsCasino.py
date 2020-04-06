801dc763c20977a888b966e10e2f12ac1ad1421f
Azerxim <mallet.maxime@hotmail.fr>
2020-04-06 18:22:15 +0200

    roulette plate

--- gems/gemsCasino.py
+++ gems/gemsCasino.py
@@ -29,6 +29,23 @@ class GemsCasino(commands.Cog):
         else:
             await ctx.channel.send(desc[2])
 
+    @commands.command(pass_context=True)
+    async def roulette(self, ctx, mise, valeur):
+        """**[bet] [value]** | Flat roulette"""
+        ID = ctx.author.id
+        param = dict()
+        param["ID"] = ID
+        param["valeur"] = valeur
+        param["mise"] = mise
+        ge.socket.send_string(gg.std_send_command("roulette", ID, ge.name_pl, param))
+        desc = GF.msg_recv()
+        lang = desc[1]
+        if desc[0] == "OK":
+            msg = desc[2]
+            await ctx.channel.send(msg)
+        else:
+            await ctx.channel.send(desc[2])
+
 
 def setup(bot):
     bot.add_cog(GemsCasino(bot))
