9d5aefc5fb16930dcc8e6b88f0b158348004f3c3
Azerxim <mallet.maxime@hotmail.fr>
2020-04-08 14:30:15 +0200

    Update level.py

--- core/level.py
+++ core/level.py
@@ -10,22 +10,25 @@ async def checklevel(message):
     nom = message.author.name
     ge.socket.send_string(gg.std_send_command("level", ID, ge.name_pl))
     desc = GF.msg_recv()
-    if desc[0] == "Level UP":
-        print("Level UP >> {0}".format(nom))
-        title = "Level UP | Get Gems"
-        lvl_desc = ":tada: {0} {1}".format(nom, desc[1])
-        msg = discord.Embed(title = title, color= 6466585, description = lvl_desc, timestamp=dt.datetime.now())
-        msg.set_thumbnail(url=message.author.avatar_url)
-        msg.set_footer(text=message.author.name)
-        await message.channel.send(embed = msg)
-    if len(desc) > 2:
-        if desc[0] != "Error":
-            title = "Success"
-            descS = ""
-            for i in range(1, len(desc[2])):
-                descS += "{0}\n".format(desc[2][i])
-            msg = discord.Embed(title = title, color= 6824352, description = descS, timestamp=dt.datetime.now())
+    try:
+        if desc[0] == "Level UP":
+            print("Level UP >> {0}".format(nom))
+            title = "Level UP | Get Gems"
+            lvl_desc = ":tada: {0} {1}".format(nom, desc[1])
+            msg = discord.Embed(title = title, color= 6466585, description = lvl_desc, timestamp=dt.datetime.now())
             msg.set_thumbnail(url=message.author.avatar_url)
             msg.set_footer(text=message.author.name)
             await message.channel.send(embed = msg)
+        if len(desc) > 2:
+            if desc[0] != "Error":
+                title = "Success"
+                descS = ""
+                for i in range(1, len(desc[2])):
+                    descS += "{0}\n".format(desc[2][i])
+                msg = discord.Embed(title = title, color= 6824352, description = descS, timestamp=dt.datetime.now())
+                msg.set_thumbnail(url=message.author.avatar_url)
+                msg.set_footer(text=message.author.name)
+                await message.channel.send(embed = msg)
+    except KeyError:
+        pass
     return False
